9b87d6a6ac20d3d82d861e8f8e9c803ce2453bc3
Samson Adewumi <samopzz@gmail.com>
2024-04-24 04:44:34 +0100

    server updated

--- server.js
+++ server.js
@@ -5,17 +5,23 @@ import winston from "winston";
 const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/BLOGGING_API";
 const PORT = process.env.PORT || 3500;
 
-connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => {
-        console.log('Connected to MongoDB successfully!');
+function connectWithRetry() {
+    return connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     })
+        .then(() => {
+            console.log('Connected to MongoDB successfully!');
+        })
 
-    .catch(err => {
-        console.error('Err connecting to MongoDB', err);
-    });
+        .catch(err => {
+            console.error('Err connecting to MongoDB', err);
+            console.log('Retrying to connect to MongoDB in 5 seconds');
+            setTimeout(connectWithRetry, 5000);
+        });
+}
+
+connectWithRetry();
 
 app.listen(PORT, () =>
     winston.info(`Server is running on port ${PORT}`)
