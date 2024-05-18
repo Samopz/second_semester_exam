4fc28a072812b097100ad2d183606024c69be1f9
Samson Adewumi <samopzz@gmail.com>
2024-05-19 00:58:57 +0100

    update to rateLimit

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const limiter = rateLimit({
   windowMs: 0.5 * 60 * 1000, // 15 minutes
   max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  legacyHeaders: true, // Disable the `X-RateLimit-*` headers
 });
 
 // Adds security middleware
