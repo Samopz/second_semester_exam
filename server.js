5f48ccf96e03cd32b8daed5fb8057eb76658bcf1
Samson Adewumi <samopzz@gmail.com>
2024-06-23 03:50:18 +0100

    update

--- server.js
+++ server.js
@@ -2,29 +2,34 @@ import app from "./src/index.js";
 import { connect } from "./src/database/connection.js";
 import winston from "winston";
 import redis from "./src/integrations/redis.js";
+import colors from "colors";
+import mongoose from "mongoose";
 
 const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT;
 redis.connect();
 
 function connectWithRetry() {
-    return connect(MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-        .then(() => {
-            console.log('Connected to MongoDB successfully!');
-        })
+  return connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      console.log(`Connected To DB ${mongoose.connection.host} `.bgYellow);
 
-        .catch(err => {
-            console.error('Err connecting to MongoDB', err);
-            console.log('Retrying to connect to MongoDB in 10 seconds');
-            setTimeout(connectWithRetry, 5000);
-        });
+      // console.log('Connected to MongoDB successfully!');
+    })
+
+    .catch((err) => {
+      console.error("Err connecting to MongoDB", err);
+      console.log("Retrying to connect to MongoDB in 10 seconds");
+      setTimeout(connectWithRetry, 5000);
+    });
 }
 
 connectWithRetry();
 
-app.listen(PORT, () =>
-    new winston.transports.Console(`Server is running on port ${PORT}`)
+app.listen(
+  PORT,
+  () => new winston.transports.Console(`Server is running on port ${PORT}`)
 );
--- src/integrations/redis.js
+++ src/integrations/redis.js
@@ -1,12 +1,28 @@
-import redis from "redis";
+// import redis from "redis";
+
+// const client = redis.createClient({
+//   host: "localhost",
+//   port: "6379",
+// });
+
+// client.on("connect", () => {
+//   console.log("Redis client connected");
+// });
+
+// export default client;
+
+import redis from 'redis';
 
 const client = redis.createClient({
-  host: "localhost",
-  port: "6379",
+    password: 'QJoN8dL9Sd59RH4HDxlL13iKfZKnOyK5',
+    socket: {
+        host: 'redis-16273.c74.us-east-1-4.ec2.redns.redis-cloud.com',
+        port: 16273
+    }
 });
 
-client.on("connect", () => {
-  console.log("Redis client connected");
+client.on('connect', () => {
+    console.log('Redis client connected to the Redis-Cloud server!');
 });
 
 export default client;
