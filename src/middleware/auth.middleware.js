2b9d53e08bb5ad76ef9040fcf46cc3fa72763736
Samson Adewumi <samopzz@gmail.com>
2024-06-30 01:38:34 +0100

    added integrations folder to .gitignore

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken';
+import JWT from 'jsonwebtoken';
 
 export function authMiddleware(req, res, next) {
     const token = req.headers['authorization'];
@@ -6,10 +6,36 @@ export function authMiddleware(req, res, next) {
         return res.status(403).json({ message: 'Authentication token required' });
     }
     try {
-        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        const payload = JWT.verify(token, process.env.JWT_SECRET);
         req.user = payload;
         next();
     } catch (err) {
         res.status(401).json({ message: 'Invalid or expired authentication token' });
     }
 }
+
+
+// export function authMiddleware (req, res, next) {
+//   try {
+//     // Get Token From Header
+//     const token = req.headers["authorization"].split(" ")[1];
+//     JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
+//       if (err) {
+//         return res.status(401).send({
+//           success: false,
+//           message: "Please provide Auth token",
+//         });
+//       } else {
+//         req.body.id = decode.id;
+//         next();
+//       }
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).send({
+//       success: false,
+//       message: "Error In Auth Middleware",
+//       error,
+//     });
+//   }
+// };
--- src/routes/blog.route.js
+++ src/routes/blog.route.js
@@ -8,15 +8,17 @@ import {
 } from "../controllers/blog.controller.js";
 import blogValidation from "../validations/blog.validation.js";
 import cacheGetAllBlogs from "../integrations/cache.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
+
 
 const router = express.Router();
 
 // Define API Endpoints for Blog
 router.post("/", blogValidation, create);
 router.get("/", getAll, cacheGetAllBlogs);
-router.get("/:id", getById);
-router.put("/:id", update);
-router.delete("/:id", deleteById);
+router.get("/:id", authMiddleware, getById);
+router.put("/:id", authMiddleware, update);
+router.delete("/:id", authMiddleware, deleteById);
 
 // Alternatively, you can use the .route() method to chain all the HTTP verbs together
 // router.route('/').get(getAll).post(create);
